e93d8b9421270f439a82600a87a994572679c4ec
mattia <mattia.federici@orobix.com>
2024-12-26 13:56:28 +0100

    first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from dvclive import Live
 import os
-os.chdir("..")
 def dvc_test():
     with Live() as live:
         live.log_artifact("./models/model.pt", type="model")
---
+++ src/main.py
@@ -0,0 +1,9 @@
+from dvclive import Live
+import os
+os.chdir("..")
+def dvc_test():
+    with Live() as live:
+        live.log_artifact("./models/model.pt", type="model")
+
+if __name__ == '__main__':
+    dvc_test()
